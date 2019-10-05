f681e950000c48741b98d91f54ceec73eb7e3c22
goldenstone111 <goldenstone111@gmail.com>
2019-10-05 11:44:52 +0530

    complete homepage ui with map integration but implementation is remaining yet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { Environment } from '@ionic-native/google-maps';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -20,6 +20,13 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
+      // Environment.setEnv({
+      //   // api key for server
+      //   'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyDP1xXi2iFeGZQCr5WS7x-GDa6Iu2LHv2E',
+
+      //   // api key for local development
+      //   'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyDP1xXi2iFeGZQCr5WS7x-GDa6Iu2LHv2E'
+      // });
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
